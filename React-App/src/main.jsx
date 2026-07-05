8365f2245dca367f7cfab07c37230144098d6858
Halima Thasleem <halimathasleem72@gmail.com>
2026-07-05 12:01:43 +0530

    Create react router.move different page in single server like home, login

--- React-App/src/main.jsx
+++ React-App/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './Login.jsx'
+import Home from './Home.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Home/>
+  },
+  {
+    path : '/login',
+    element : <Login/>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
